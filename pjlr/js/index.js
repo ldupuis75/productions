79d0062b218f079a7ae00e5245d2707286166c62
Paul Joannon <hello@pauljoannon.com>
2015-05-04 17:21:23 +0200

    [pjlr] enable navigation between sections

--- pjlr/js/index.js
+++ pjlr/js/index.js
@@ -1,11 +1,28 @@
 $(function() {
+	var activateSection = function(className) {
+		// Change colors
+		$('.wrapper').removeClass('wrapper-une-phrase wrapper-en-details wrapper-dans-la-vraie-vie wrapper-avec-des-poissons');
+		$('.wrapper').addClass('wrapper-' + className);
+
+		// Activate menu item
+		$('#menu li').removeClass('selected');
+		$('#menu li.' + className).addClass('selected');
+
+		// Change what is displayed
+		$('.pages .une-phrase').hide();
+		$('.pages .en-details').hide();
+		$('.pages .dans-la-vraie-vie').hide();
+		$('.pages .avec-des-poissons').hide();
+		$('.pages .' + className).show();
+	};
+
 	// Register menu actions
 	$('#menu li a').on('click', function(event) {
 		event.preventDefault();
-		console.debug($(this).attr('rel'))
+		activateSection($(this).attr('rel'));
 	});
 
-	$('#menu li:first-of-type').addClass('selected');
+	activateSection('une-phrase');
 
     //   $('.isotope').isotope({
     //     itemSelector : '.pages',
